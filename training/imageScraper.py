afe5392da56aab54bcb20e2e0ac2ef3a937c119c
justin-h-im <imjustin679@gmail.com>
2025-01-09 15:41:31 -0800

    updated negative inputs and imageScraper

--- training/imageScraper.py
+++ training/imageScraper.py
@@ -3,25 +3,25 @@ from icrawler.builtin import BingImageCrawler
 
 # Crawling for positive images
 print("Crawling for positives")
-positive_crawler = BingImageCrawler(parser_threads = 2, downloader_threads = 4, storage = {'root_dir': r'training/ptest'})
+positive_crawler = BingImageCrawler(parser_threads = 2, downloader_threads = 4, storage = {'root_dir': r'training/p'})
 positive_crawler.session.verify = False
 with open("training/inputspos.txt") as pinputs_file:
     for line in pinputs_file:
         positive_crawler.crawl(
             keyword = line,
-            max_num = 1000,
+            max_num = 500,
             file_idx_offset = 'auto')
         
-print("Crawling for negatives")
-# Crawling for negative images
-negative_crawler = BingImageCrawler(parser_threads = 2, downloader_threads = 4, storage = {'root_dir': r'training/ntest'})
-negative_crawler.session.verify = False
-with open("training/inputsneg.txt") as ninputs_file:
-    for line in ninputs_file:
-        negative_crawler.crawl(
-            keyword = line,
-            max_num = 1000,
-            file_idx_offset = 'auto')
+# print("Crawling for negatives")
+# # Crawling for negative images
+# negative_crawler = BingImageCrawler(parser_threads = 2, downloader_threads = 4, storage = {'root_dir': r'training/n'})
+# negative_crawler.session.verify = False
+# with open("training/inputsneg.txt") as ninputs_file:
+#     for line in ninputs_file:
+#         negative_crawler.crawl(
+#             keyword = line,
+#             max_num = 500,
+#             file_idx_offset = 'auto')
 
 # Loops to count
 print("Counting Files")
