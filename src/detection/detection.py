73e1d26001ac0f3955cefcfce9e2d5599a8629ea
justin-h-im <imjustin679@gmail.com>
2025-01-28 15:29:15 -0800

    beginning to implement multithreading

--- src/detection/detection.py
+++ src/detection/detection.py
@@ -1,26 +1,22 @@
 import cv2
 import numpy as np
 import time
+import threading
+from queue import Queue
 from paho.mqtt import client as mqtt_client
 
-# This script is the publisher for mqtt
-
-stream_url = "http://10.42.0.107:7123/stream.mjpg"
-cap = cv2.VideoCapture(stream_url)
-
-# Cascade data for traffic cones
-cone_data = cv2.CascadeClassifier('/home/justin-im/Projects/auto-drone/training/classifier/cascade1.xml')
-
-# Client and broker set up
-broker = 'broker.emqx.io'
-port = 1883
-topic = "Drone Commands"
-client_id = f'python-mqtt-{"jiggles"}'
-
 # Array of messages?
-msg_array = np.array(["Move Forward", "Move Left", "Move Right", "Move Back", "Move Up", "Move Down"])
+msg_array = np.array([000, 001, 010, 011, 100, 101])
+frame_queue = Queue(maxsize=30)
+result_queue = Queue()
 
 def connect_mqtt():
+    global topic
+
+    broker = 'broker.emqx.io'
+    port = 1883
+    topic = "Drone Commands"
+    client_id = f'python-mqtt-{"jiggles"}'
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
@@ -44,13 +40,56 @@ def publish(client, msg):
     else:
         print(f"Failed to send message to topic {topic}")
 
+def init():
+    # Client and broker set up
+
+    global client
+    global cap
+    global cone_data
+
+    client = connect_mqtt()
+    client.loop_start
+    # Delay so that local host can set up
+    time.sleep(20)
+    stream_url = "http://10.42.0.107:7123/stream.mjpg"
+    cap = cv2.VideoCapture(stream_url)
+    # Cascade data for traffic cones
+    cone_data = cv2.CascadeClassifier('/home/justin-im/Projects/auto-drone/training/classifier/cascade1.xml')
+
+def frameCapture():
+    counter = 0
+    while(1):
+        ret, frame = cap.read()
+        cv2.imshow("capture", frame)
+        if not ret:
+            print("Frame grab error")
+
+        # Saves 1 frame out of 10 frames into the queue for analysis
+        if counter%10 == 0:
+            frame_queue.put(frame)
+        else:
+            print("Full frame queue, dropping frame")
+        
+def frameAnalysis():
+    while(1):
+        if not frame_queue.empty():
+            frame = frame_queue.get()
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            found = cone_data.detectMultiScale(gray_frame, minSize = (24, 24), minNeighbors=3)
+            result_queue.put(found)
+        else:
+            time.sleep(0.01)
+
+def sendData():
+    while(1):
+        if not result_queue.empty():
+            box = result_queue.get()
+            amount_found = len(box)
+            if amount_found > 0:
+
+
 
 def run():
-    # Counter value
-    counter = 0
-    client = connect_mqtt()
-    client.loop_start()
-
     while(1):
 
         # Array of CV2 Rectangles dimensions
