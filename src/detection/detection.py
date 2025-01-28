b1f52e8141600a2bcc8f06c6d88824ebda05c6bf
justin-h-im <imjustin679@gmail.com>
2025-01-28 15:45:57 -0800

    untested multithreading configuration

--- src/detection/detection.py
+++ src/detection/detection.py
@@ -6,9 +6,12 @@ from queue import Queue
 from paho.mqtt import client as mqtt_client
 
 # Array of messages?
-msg_array = np.array([000, 001, 010, 011, 100, 101])
+msg_array = np.array([0b000, 0b001, 0b010, 0b011, 0b100, 0b101])
 frame_queue = Queue(maxsize=30)
 result_queue = Queue()
+time.sleep(20)
+stream_url = "http://10.42.0.107:7123/stream.mjpg"
+cap = cv2.VideoCapture(stream_url)
 
 def connect_mqtt():
     global topic
@@ -29,7 +32,6 @@ def connect_mqtt():
     client.connect(broker, port)
     return client
 
-
 def publish(client, msg):
     time.sleep(1)
     result = client.publish(topic, msg)
@@ -44,15 +46,11 @@ def init():
     # Client and broker set up
 
     global client
-    global cap
     global cone_data
 
     client = connect_mqtt()
     client.loop_start
-    # Delay so that local host can set up
-    time.sleep(20)
-    stream_url = "http://10.42.0.107:7123/stream.mjpg"
-    cap = cv2.VideoCapture(stream_url)
+
     # Cascade data for traffic cones
     cone_data = cv2.CascadeClassifier('/home/justin-im/Projects/auto-drone/training/classifier/cascade1.xml')
 
@@ -60,22 +58,28 @@ def frameCapture():
     counter = 0
     while(1):
         ret, frame = cap.read()
-        cv2.imshow("capture", frame)
         if not ret:
             print("Frame grab error")
+
+        cv2.imshow("capture", frame)
 
         # Saves 1 frame out of 10 frames into the queue for analysis
         if counter%10 == 0:
             frame_queue.put(frame)
         else:
             print("Full frame queue, dropping frame")
+        counter+=1
+        
+    
         
 def frameAnalysis():
     while(1):
         if not frame_queue.empty():
             frame = frame_queue.get()
             gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            found = cone_data.detectMultiScale(gray_frame, minSize = (24, 24), minNeighbors=3)
+            found = cone_data.detectMultiScale(gray_frame, minSize = (24, 24), scaleFactor=1.1, minNeighbors=3)
+            for (x,y,w,h) in found:
+                cv2.rectangle(frame, (x, y), (x+2, y+h), (255, 0, 0), 2)
             result_queue.put(found)
         else:
             time.sleep(0.01)
@@ -86,88 +90,16 @@ def sendData():
             box = result_queue.get()
             amount_found = len(box)
             if amount_found > 0:
+                sorted_rects = sorted(box, key=lambda r: r[2]*r[3], reverse = True)
+                center = (sorted_rects[0][0] + int(sorted_rects[0][2]/2, sorted_rects[0][1] + int(sorted_rects[0][3]/2)))
 
-
-
-def run():
-    while(1):
-
-        # Array of CV2 Rectangles dimensions
-        max_list = np.empty((10,4))
-        
-        # Captures the frame and stores found object rectangles in found
-        ret, frame = cap.read()
-        cv2.imshow("capture", frame)
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        found = cone_data.detectMultiScale(gray_frame, minSize = (24, 24))
-        amount_found = len(found)
-        print(f"Amount Found: {amount_found}")
-        
-        # Finds the rectangle with the largest area and appends it to our list
-        if amount_found > 0:
-            # Get the first maximum area
-            max = np.array([found[0][0], found[0][1], found[0][2], found[0][3]])
-            max_area = found[0][2] * found[0][3]
-            
-            # Checks the rest of the found rectangles for the maximum area
-            for i in range(amount_found):
-                area = found[i][2] * found[i][3]
-                if max_area < area:
-                    max = np.array([found[i][0], found[i][1], found[i][2], found[i][3]])
-                    max_area = area
-            
-            # Populates the correct row of max_list with the values of the maximum rectangle
-            for i in range(4):
-                max_list[counter][i] = max[i]
-                
-            counter = counter + 1
-            print(f"Counter: {counter}")
-            
-            # Calculate Average X, Y
-            if counter == 10:
-                avgx, avgy, avgw, avgh = 0, 0, 0, 0
-                for i in range(10):
-                    avgx += max_list[i][0]
-                    avgy += max_list[i][1]
-                    avgw += max_list[i][2]
-                    avgh += max_list[i][3]
-                
-                avgx = int(avgx/10)
-                print(f"Average x: {avgx}")
-                avgy = int(avgy/10)
-                print(f"Average y: {avgy}")
-                avgw = int(avgw/10)
-                print(f"Average w: {avgw}")
-                avgh = int(avgh/10)            
-                print(f"Average h: {avgh}")
-            
-                # Display most recent frame with average x, y, height, and width
-                cv2.rectangle(frame, (int(avgx), int(avgy)), (int(avgx+avgw), int(avgy+avgh)), (255, 0, 0), 2)
-                cv2.imshow("capture", frame)
-                
-                # Find center of the object
-                object_center_x = avgx + int(avgw/2)
-                object_center_y = avgy + int(avgh/2)
-                
-                
-                # Ignore instances where the average area is less than a something, arbitrarily chose 50 x 50
-                msg = msg_array[0]
-                if (avgx > 100 and avgy > 100):
-                    # Based on center coordinates relative to center of 640x480p camera feed, switch statement to make a decision
-                    if (avgx > 320):
-                        # Move Right
-                        msg = msg_array[2]
-                    else:
-                        # Move Left (also by default)
-                        msg = msg_array[1]
-
-                print(f"Direction Message: {msg}")
-                
-                # Broadcast message
-                publish(client, msg)
-    
-                # Reset counter
-                counter = 0
+                if center[0] > 320:
+                    msg = 1
+                    publish(client, msg)
+                else:
+                    msg = 2
+                    publish(client, msg)
+                    
     
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -177,4 +109,24 @@ def run():
     client.loop_stop()
 
 if __name__ == '__main__':
-    run()
+    init_thread = threading.Thread(target = init)
+    time.sleep(2)
+    frameCapture_thread = threading.Thread(target = frameCapture)
+    time.sleep(2)
+    frameAnalysis_thread = threading.Thread(target = frameAnalysis)
+    time.sleep(2)
+    sendData_thread = threading.Thread(target = sendData)
+    time.sleep(2)
+
+    init_thread.start()
+    frameCapture_thread.start()
+    frameAnalysis_thread.start()
+    sendData_thread.start()
+
+    init_thread.join()
+    frameCapture_thread.join()
+    frameAnalysis_thread.join()
+    sendData_thread.join()
+
+    print("Threads Completed")
+    
